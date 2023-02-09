d3775acb8c2ecbcc3c1dee7e7bf92092f60187e0
Koka <kshurgaya91@gmail.com>
2023-02-10 01:00:04 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double random()
+{
+    double number = new Random().Next(0, 11) + new Random().NextDouble();
+    System.Console.Write(number + " ");
+    return number;
+}
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random();
+}
+
+void PrintArray(double[] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+(double, double) MinMix(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return (min,max);
+}
+System.Console.WriteLine();
+(double min, double max) = MinMix(array);
+PrintArray(array, $"Минимальное = {min}, максимальное {max}");
